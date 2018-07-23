f96b1c5b687fba19cd3b77e38a510734ab9641f4
freewind <nowindlee@gmail.com>
2018-07-23 10:25:28 +0800

    alias name demo

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Get Jdbc ResultSet Column Names Demo
 ====================================
 
-Use h2 database for demo.
+Use h2 database for demo, get columeName and displayName(alias name) from `ResultSet`.
 
 See `Hello.kt` file and run it.
--- src/main/kotlin/example/Hello.kt
+++ src/main/kotlin/example/Hello.kt
@@ -1,5 +1,6 @@
 package example
 
+import java.sql.Connection
 import java.sql.DriverManager
 import java.sql.ResultSet
 
@@ -14,33 +15,59 @@ fun main(args: Array<String>) {
 
         conn.createStatement().use { stmt ->
             with(stmt) {
-                executeUpdate("create table mytbl(id int primary key, name varchar(255))")
-                executeUpdate("insert into mytbl values(1, 'Hello')")
-                executeUpdate("insert into mytbl values(2, 'World')")
+                executeUpdate("create table users(id int primary key, name varchar(255))")
+                executeUpdate("insert into users values(1, 'Hello')")
+                executeUpdate("insert into users values(2, 'World')")
             }
         }
 
-        conn.createStatement().use { stmt ->
-            val rs = stmt.executeQuery("select * from mytbl")
-            val columnNames = getColumnNames(rs)
+        printColumnNames(conn)
 
-            columnNames.forEach { name -> print("$name\t") }
-            println()
-
-            while (rs.next()) {
-                columnNames.forEach { name ->
-                    print(rs.getObject(name).toString() + "\t")
-                }
-                println()
-            }
-        }
+        printAliasNames(conn)
     }
 
+}
+
+private fun printColumnNames(conn: Connection) {
+    conn.createStatement().use { stmt ->
+        val rs = stmt.executeQuery("select * from users")
+
+        getColumnNames(rs).forEach { name -> print("$name\t") }.also {
+            println()
+        }
+
+        printRsRows(rs, getColumnNames(rs))
+    }
+}
+
+private fun printAliasNames(conn: Connection) {
+    conn.createStatement().use { stmt ->
+        val rs = stmt.executeQuery("select id as id_alias, name as name_alias from users")
+        getColumnAliasNames(rs).forEach { name -> print("$name\t") }.also {
+            println()
+        }
+
+        printRsRows(rs, getColumnAliasNames(rs))
+    }
+}
+
+private fun printRsRows(rs: ResultSet, columnNames: List<String>) {
+    while (rs.next()) {
+        columnNames.forEach { name ->
+            print(rs.getObject(name).toString() + "\t")
+        }
+        println()
+    }
 }
 
 private fun getColumnNames(rs: ResultSet): List<String> {
     val metaData = rs.metaData
     return (1..metaData.columnCount).map { metaData.getColumnName(it) }.toList()
+}
+
+private fun getColumnAliasNames(rs: ResultSet): List<String> {
+    val metaData = rs.metaData
+    return (1..metaData.columnCount).map { metaData.getColumnLabel(it) }.toList()
 }
 
 fun <T : AutoCloseable?, R> T.use(block: (T) -> R): R {
